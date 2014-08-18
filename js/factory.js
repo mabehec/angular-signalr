d6217a99f60a503f8fe00a1355a6b3c13f0557be
mabehec <mabehec@gmail.com>
2014-08-18 11:41:50 -0400

    commit

---
+++ js/factory.js
@@ -0,0 +1,11 @@
+var usine = angular.module('ic_usine',['ngResource'])
+
+//usine.factory('createEvent', function($resource){
+//    return{
+//        creer: $resource('http://ticketingreservations.azurewebsites.net/api/events/provisioning/admissions/ga', null,
+//            {'post':  {method:'POST'}})
+//
+//
+//}});
+
+
